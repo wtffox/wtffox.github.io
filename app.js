b47617bcb3cc7b028c3a755a962c46dbb504a403
wtffox <wtffox@users.noreply.github.com>
2020-03-07 22:45:57 +0300

    inital commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var formValue = [];
+var inputElem = document.getElementById('string');
+var outputElementS = document.getElementById('text1');
+var outputElementK = document.getElementById('text');
+var formatedValue = [];
+var outputPatternS = 's@i.com';
+var outputPatternK = 'k@i.com';
+
+var inp = document.querySelector("[type='button']");
+inp.addEventListener('click', add)
+function add(){
+  let value = inputElem.value;
+  checkUsInp(value);
+  value = toCurrectLength(value);
+  formValue.push(value);
+  toFormat(value);
+  outputTo(outputElementS, formValue)
+  outputTo(outputElementK, formatedValue)
+};
+function toFormat(element){
+  formatedValue.push(element.concat(outputPatternS))
+  formatedValue.push(element.concat(outputPatternK))
+};
+function checkUsInp(value){
+  const reg = new RegExp("/\d");
+if (!value.match(reg)){
+  console.log(value.match("\d"))
+  console.log('debil')
+};
+};
+
+function toCurrectLength(value){
+  let currentLength =  value.length;
+  console.log(currentLength)
+  let result = value;
+  while(currentLength <5){
+    result = "0".concat(result);
+    currentLength++;
+  }
+  return result;
+}
+
+function outputTo(outputToId, value){
+  outputToId.value = value;
+}
+// нажатие на Enter
+inputElem.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("myBtn").click();
+  }
+});
